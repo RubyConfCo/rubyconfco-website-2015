d1020333ebebda518b7b4278a4bc43fbb5f6793d
Miguel Caro <miguel.caro@koombea.com>
2015-04-15 17:49:34 -0500

    added map to venue section

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -31,3 +31,27 @@ $(".js-scrollTo").on('click', function(e){
   var sectionPosition = $(sectionName).offset().top;
   $("html, body").animate({scrollTop : sectionPosition},1000);
 })
+
+// MAP
+
+$('.js-map').on('click', function(event) {
+  event.preventDefault();
+  $('.map-overlay').addClass('open');
+});
+
+$('.js-map-close').on('click', function(event) {
+  event.preventDefault();
+  $('.map-overlay').removeClass('open');
+});
+
+function initialize() {
+  var mapCanvas = document.getElementById('map-canvas');
+  var mapOptions = {
+    center: new google.maps.LatLng(44.5403, -78.5463),
+    zoom: 8,
+    disableDefaultUI: true,
+    mapTypeId: 'roadmap'
+  }
+  var map = new google.maps.Map(mapCanvas, mapOptions)
+}
+google.maps.event.addDomListener(window, 'load', initialize);
