18a01989247a15a8ddfe48f14749ea6294788bcf
Miguel Caro <miguel.caro@koombea.com>
2015-04-15 17:02:00 -0500

    scrollTo, accordion

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -1,5 +1,7 @@
 //= require_tree .
 
+//  HEADER 
+
 $(window).scroll(function() {
   var scroll = $(this).scrollTop();
   var hbanner = $('.main-banner').height();
@@ -10,3 +12,22 @@ $(window).scroll(function() {
     $('.header').removeClass('onsight');
   }
 });
+
+//  ACCORDION 
+
+var accordion = $(".js-accordion");
+var accordionTrigger = $(".js-accordion").find('.accordion__header');
+
+accordion.find('.accordion__body').hide();
+
+$(accordionTrigger).on('click', function() {
+  $(this).toggleClass('open');
+  $(this).next('.accordion__body').slideToggle();
+}); 
+
+$(".js-scrollTo").on('click', function(e){
+  e.preventDefault();
+  var sectionName = $(this).attr('data-scrollto');
+  var sectionPosition = $(sectionName).offset().top;
+  $("html, body").animate({scrollTop : sectionPosition},1000);
+})
