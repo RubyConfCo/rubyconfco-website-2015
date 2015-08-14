9dc0baf5519e38c8e7ac46b121e2dd166e9603f9
robertomiranda <rjmaltamar@gmail.com>
2015-08-14 17:41:51 -0500

    Add Ambassadors program

--- config.rb
+++ config.rb
@@ -19,6 +19,7 @@
 # With alternative layout
 page "/code_of_conduct.html", :layout => :otherlayout
 page "/estudiantes.html", :layout => :otherlayout
+page "/ambassadors.html", :layout => :otherlayout
 #
 # A path which all have the same layout
 # with_layout :admin do
